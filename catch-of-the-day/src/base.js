1ffb7800e80e3294bec0790d1d050270296a91b4
Hieu Nguyen <hnnguyen45@gmail.com>
2017-02-13 18:51:19 -0800

    second commit

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCpAcZYkjxUMiXXZARiUyWBBRVvDZf6JF0",
+    authDomain: "catch-of-the-day-hieu-nguyen.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-hieu-nguyen.firebaseio.com",
+});
+
+export default base;
